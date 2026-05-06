4dd1c1c67914e7ca43e5622d3d67e1fd14b2e4c5
ELBA21 <benjaminalberto.martinez@alumnos.ulagos.cl>
2026-05-06 02:40:59 -0400

    feat: Se agrega EBS (casi)

--- infrastructura/deploy.sh
+++ infrastructura/deploy.sh
@@ -40,11 +40,12 @@ aws ec2 modify-vpc-attribute --vpc-id $VPC_ID --enable-dns-hostnames
 
 
 # ================================
-# SECCION 2: CREAR SUBNETS
+# SECCION 2: CREAR SUBNET
 # ================================
 echo "Iniciando creacion de subnet"
-# Creamos una subnet en el mismo rango de la VPC, esta sera la subnet Publica 
+# Creamos una subnet en el mismo rango de la VPC, esta sera la subnet Publica, avilitada en la region, hardcodeamos que sea 'us-east-1a'
 export SUBNET_PUB=$(aws ec2 create-subnet --vpc-id $VPC_ID --cidr-block 10.0.1.0/24 --availability-zone ${REGION}a --query 'Subnet.SubnetId' --output text)
+# captura de id a ver si hay error, si no veo mensajes conforma pasa el tiempo me da ansiedad
 if [ -z "$SUBNET_PUB" ] || [ "$SUBNET_PUB" == "None" ]; then
     echo "ERROR: No se pudo crear la SUBNET Publica."
     exit 1
@@ -52,13 +53,15 @@ if [ -z "$SUBNET_PUB" ] || [ "$SUBNET_PUB" == "None" ]; then
 else
     echo "OK: SUBNET Publica creada con ID: $SUBNET_PUB"
 fi
+
 # Aprovecho que VPC_NAME es lab1 para asignar nombres de forma mas consistente
 aws ec2 create-tags --resources $SUBNET_PUB --tags Key=Name,Value=${VPC_NAME}-Subnet-Pub
 # Asigna Ip publicas automaticamente
 aws ec2 modify-subnet-attribute --subnet-id $SUBNET_PUB --map-public-ip-on-launch
 
-# Creamos otra subnet, igual ene l mismo rango de la VPC, esta sera la privada
+# Creamos otra subnet, igual ene l mismo rango de la VPC, esta sera la privada, esta esta harcodeada para estar en la misma region, pero otra zona
 export SUBNET_PRIV=$(aws ec2 create-subnet --vpc-id $VPC_ID --cidr-block 10.0.2.0/24 --availability-zone ${REGION}b --query 'Subnet.SubnetId' --output text)
+# comprobacion
 if [ -z "$SUBNET_PRIV" ] || [ "$SUBNET_PRIV" == "None" ]; then
     echo "ERROR: No se pudo crear la SUBNET Privada"
     exit 1
@@ -75,9 +78,10 @@ aws ec2 create-tags --resources $SUBNET_PRIV --tags Key=Name,Value=${VPC_NAME}-S
 # SECCION 3: CREAR IGW
 # ================================
 echo "Iniciando creacion de Internet Gateway"
-# Creamos una internet gateway, usamos wait para esperar  la respuesta
+# Creamos una internet gateway, nada mas que comentar aca
 export IGW_ID=$(aws ec2 create-internet-gateway --query 'InternetGateway.InternetGatewayId' --output text)
 
+# Capturar de errores
 if [ -z "$IGW_ID" ] || [ "$IGW_ID" == "None" ]; then
     echo "ERROR: No se pudo crear el Internet Gateway"
     exit 1
@@ -97,7 +101,9 @@ aws ec2 attach-internet-gateway --internet-gateway-id $IGW_ID --vpc-id $VPC_ID
 # SECCION 4: Route Tables
 # ================================
 echo "Iniciando creacion de Route Tables"
+# Le decimos a aws que la route table corresponde a la VPC que creamos en este archivo ya
 export RT_ID=$(aws ec2 create-route-table --vpc-id $VPC_ID --query 'RouteTable.RouteTableId' --output text)
+# comprobacion de errores, bueno, de NO errores (se me acaban las frases)
 if [ -z "$RT_ID" ] || [ "$RT_ID" == "None" ]; then
     echo "ERROR: No se pudo crear el Route Table"
     exit 1
@@ -115,29 +121,39 @@ aws ec2 associate-route-table --route-table-id $RT_ID --subnet-id $SUBNET_PUB
 
 
 # ================================
-# SECCION 5: Security Group e Instancia
+# SECCION 5: Security Group
 # # ================================
 
-# Creamos el security group'y guardamos su id
-export SG_ID=$(aws ec2 create-security-group --group-name sg-lab --description "lab SG" --vpc-id $VPC_ID --query 'GroupId' --output text)
-
+# Creamos el security group y guardamos su id
+# Le ponemos un nombre, como curiosidad al principio lo llame 'sg-lab', amazon da error automatico con eso
+# El SG es asignado a la VPC ya creada
+export SG_ID=$(aws ec2 create-security-group --group-name lab1-security-group --description "lab SG" --vpc-id $VPC_ID --query 'GroupId' --output text)
+# comprobar
 if [ -z "$SG_ID" ] || [ "$SG_ID" == "None" ]; then
     echo "ERROR: No se pudo crear el Security Group"
     exit 1
-
 else
     echo "OK:Security Group creado con ID: $SG_ID"
 fi
-# Abrimos el puerto 80 al internet
+# Abrimos el puerto HTTP (80) al internet
 aws ec2 authorize-security-group-ingress --group-id $SG_ID --protocol tcp --port 80 --cidr 0.0.0.0/0
-# Dejamos el puerto 22 disponible unicamente para nuestra Ip personal
+# Dejamos el puerto ssh (22) disponible unicamente para nuestra Ip personal
 aws ec2 authorize-security-group-ingress --group-id $SG_ID --protocol tcp --port 22 --cidr $(curl -s ifconfig.me)/32 
+# ================================
+# SECCION 6: Instancia
+# ================================
+
 
 # En un principio la image-id estaba hardcodeada, la IA recomienda que se obtenga de forma dinamica, pq puede cambiar segun la region o el tiempo
 # Personalmente no creo que cambie, pero soy paranoico, que pasa si del viernes al domingo amazon la cambia? Pues nada, hay que prevenir
-export AMI_ID=$(aws ec2 describe-images --owners 099720109477 --filters "Name=name,Values=ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*" --query 'sort_by(Images, &CreationDate)[-1].ImageId' --output text)
-# otra captura de erroes mas
+# solicitamos la id de la imagen indicando la id correspondiente a cannonical, quienes son los dueños de ubuntu
+# Solicitamos la ami_id de ubuntu-24 especificamente.
+export AMI_ID=$(aws ec2 describe-images \
+    --owners 099720109477 \
+    --filters "Name=name,Values=ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*" \
+    --query 'sort_by(Images, &CreationDate)[-1].ImageId' --output text)
 
+# otra captura de erroes mas
 if [ -z "$AMI_ID" ] || [ "$AMI_ID" == "None" ]; then
     echo "ERROR: No se pudo obtener la image-id"
     exit 1
@@ -145,8 +161,13 @@ if [ -z "$AMI_ID" ] || [ "$AMI_ID" == "None" ]; then
 else
     echo "OK: obtenemos imagen id: $AMI_ID"
 fi
-# Levantamos una instancia de t3.micro con Ubuntu server 26.04 (teoricamente deberia ser)
-# Unico comando que hago saltar lineas pq estaba ilegible
+# Levantamos una instancia de t3.micro con Ubuntu server 24.04 (teoricamente deberia ser)
+# Aca creamos la instancia, le damos la imagen del SO que instalaremos, solicitamos que su capacidad de computo sea t3.micro
+# La conectamos a la subnet publica, adeas de conectarla al grupo de seguridad configurado antes
+# aslkdjas
+# le digo que funciona con la key ya creada y registrada en linea
+# Por ultimo comandos para ejecutar cosas DENTRO de la maquinita
+echo "Generando Instancia"
 export INSTANCE_ID=$(aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type t3.micro \
@@ -154,12 +175,77 @@ export INSTANCE_ID=$(aws ec2 run-instances \
     --security-group-ids $SG_ID \
     --associate-public-ip-address \
     --key-name keybenja \
+    --user-data '#!/bin/bash
+    # Actualizamos repositorios apt, la "y" hace que se responda si automaticamente (finalmente la considero util)
+    apt-get update -y
+    # instalamos apache
+    apt-get install -y apache2' \
     --query 'Instances[0].InstanceId' --output text)
 
+# Crear volumen en la misma AZ que la EC2
+# size 8 significa 8 gigas, mientras gp3 significa que se usara un SSD general
+# notar que el availability-zone es el mismo de la instancia para que se puedan conectar
+echo "Generando volumen"
+export VOL_ID=$(aws ec2 create-volume \
+    --size 8 \
+    --volume-type gp3 \
+    --availability-zone ${REGION}a \
+    --query 'VolumeId' --output text)
+
+# Esperamos que el disco este listo
+echo "Esperando volumen"
+aws ec2 wait volume-available --volume-ids $VOL_ID
+
+# Esperamos que la instancia este running
+echo "Esperando instancia"
 aws ec2 wait instance-running --instance-ids $INSTANCE_ID
 
-# SECCION: VERIFICAR!!!!!
-
+# Adjuntamos disco a la instancia
+echo "Adjuntando instancia y disco"
+aws ec2 attach-volume \
+    --volume-id $VOL_ID \
+    --instance-id $INSTANCE_ID\
+    --device /dev/xvdf
+echo "Disco adjuntado"
 # Obtenemos la IP publica de la maquina y esperamos respuesta!
-export PUBLIC_IP=$(aws ec2 describe-instances --filters Name=subnet-id,Values=$SUBNET_PUB --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+echo "Obteniendo IP publica"
+export PUBLIC_IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
 ping -c 4 $PUBLIC_IP
+
+echo "Ejecutando ssh"
+ssh -i keybenja.pem ubuntu@$PUBLIC_IP << 'EOF'
+    # en un principio hardcodee un timing para esperar a apache, pero era inseguro
+    # La ia me recomendo este comando mejor, consulta al sistema si existe apache en ciclos de 3 segundos
+    while ! systemctl is-active --quiet apache2; do sleep 3; done
+    
+    # vale hay que buscar el nombre que tiene el disco internamente
+    # basicamente filtrmoas en la lista de discos y tomamos el que esta en la 'cabeza' jeje (son las 2am)
+    DISK=$(lsblk -dnpo NAME,SIZE | grep "8G" | awk '{print $1}' | head -n 1)
+    echo "Disco es: $DISK"
+
+
+    # Formateamos el disco a formato ext4 
+    sudo mkfs -t ext4 "$DISK"
+
+    # creamos una carpeta y dejamos que sea el acceso al disco
+    sudo mkdir -p /mnt/datos
+    sudo mount "$DISK" /mnt/datos
+
+
+    # creamos carpeta dentro del EBS
+    sudo mkdir -p /mnt/datos/www-content
+
+    # TEST BORRAR TEST
+    echo "<h1>Laboratorio</h1>" | sudo tee /mnt/datos/www-content/index.html
+
+    # persistencia de discos, para que no se borren al reiniciar ni nada, aunque dudo que esto se reinicie, tipo solo servira una vez prendido
+    echo "$DISK /mnt/datos ext4 defaults,nofail 0 2" | sudo tee -a /etc/fstab
+    echo "/mnt/datos/www-content /var/www/html none defaults,bind 0 0" | sudo tee -a /etc/fstab
+    
+    # montaje bind
+    # basicamente hago que la carpeta donde guardamos la pagina en el ebs este conectada a la carpeta que lee apache
+    sudo mount --bind /mnt/datos/www-content /var/www/html
+
+    # reiniciamos apache
+    sudo systemctl restart apache2
+EOF
